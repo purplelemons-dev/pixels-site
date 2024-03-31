bfe9b230b8dcd4581bc7df167a7437b9c92ba06a
purplelemons-dev <ironbreaker2005@gmail.com>
2024-03-31 18:04:33 -0500

    Add color selector and grid to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
-<h1></h1>
+<script lang="ts">
+	import ColorSelector from "$lib/components/ColorSelector.svelte"
+	let currentColor = "black";
+	const cellClicked = (me: MouseEvent) => {
+		(me.currentTarget as HTMLTableCellElement).style.backgroundColor = currentColor;
+	};
+</script>
+
+<table >
+	<tbody>
+		{#each Array(50) as _, i}
+			<tr>
+				{#each Array(118) as _, j}
+					<td on:click={cellClicked}></td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<ColorSelector bind:currentColor />
+
+<style>
+	table {
+		border-spacing: 0;
+		width: calc(118 * 8px);
+		height: calc(50 * 8px);
+		table-layout: fixed;
+	}
+	td {
+		width: 8px;
+		height: 8px;
+		border: 1px solid black;
+		background-color: black;
+	}
+</style>
