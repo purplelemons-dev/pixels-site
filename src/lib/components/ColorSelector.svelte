e17ddb89a4722653c4d93ba237073b7340a11981
purplelemons-dev <ironbreaker2005@gmail.com>
2024-03-31 18:04:48 -0500

    Add ColorSelector component to project

---
+++ src/lib/components/ColorSelector.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let currentColor: string;
+
+	const colorNames = ['black', 'red', 'green', 'blue', 'yellow', 'purple', 'cyan', 'white'];
+</script>
+
+<div class="color-selector">
+	{#each colorNames as colorName, idx}
+		<div
+			class="color"
+			style={`background-color: ${colorName}`}
+			on:click={() => (currentColor = colorName)}
+			tabindex="{idx}"
+		></div>
+	{/each}
+</div>
